0d4a2fe1a540f1442ab9accd2016ccf0fd12df70
Eyeseas <konglinghu777@gmail.com>
2024-04-18 22:47:00 +0800

    feat: 柯里化

--- Curry/curry.js
+++ Curry/curry.js
@@ -1,35 +1,28 @@
-// function Curry(fn) {
-//     let fnArgLength = fn.length;
-//     let mergeArgs = [];
-
-//     function next(...extArgs) {
-//         mergeArgs = [...mergeArgs, ...extArgs];
-//         console.log(mergeArgs.length, fnArgLength);
-//         if (mergeArgs.length < fnArgLength) {
-//             return next;
-//         } else {
-//             return fn.apply(this, mergeArgs.slice(0, fnArgLength.length));
-//         }
-//     }
-
-//     return next;
-// }
-
-function Curry(fn) {
+function curry(fn) {
     return function curried(...args) {
+        // 检查当前收集到的参数数量是否足够原函数执行
         if (args.length >= fn.length) {
-            return fn.apply(this, args);
+            // 如果足够，直接执行原函数，但只传入原函数所需数量的参数
+            return fn.apply(this, args.slice(0, fn.length));
         } else {
+            // 如果不够，返回一个新的函数，继续收集参数
             return function (...args2) {
+                // 递归调用curried，合并之前和当前的参数
                 return curried.apply(this, args.concat(args2));
             };
         }
     };
 }
-const add = (a, b, c, d, e) => {
-    return a + b + c + d + e;
-};
 
-const newAdd = Curry(add);
+// 示例：使用curry函数柯里化一个三参数的加法函数
+function add(a, b, c) {
+    return a + b + c;
+}
 
-console.log(newAdd(1, 2)(2)(3)(4,5));
+let curriedAdd = curry(add);
+
+console.log(curriedAdd(1, 2, 3)); // 输出 6
+console.log(curriedAdd(1)(2)(3)); // 输出 6
+console.log(curriedAdd(1, 2)(3)); // 输出 6
+console.log(curriedAdd(1)(2, 3)); // 输出 6
+console.log(curriedAdd(1, 2, 3, 4)); // 输出 6，忽略多余的参数
